e15db3598b363000fd76330f891b3ae4e49dc478
mnfmnfm <mnfmnfm@gmail.com>
2021-06-16 11:33:29 -0700

    finish frontend code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+  onFormSubmit = async (e) => {
+    e.preventDefault();
+    let pictureData = await axios.get(`http://localhost:3002/pictures?searchQuery=${e.target.query.value}`);
+
+    console.log(pictureData);
+
+    this.setState({
+      pictures: pictureData.data
+    });
+  }
+  render() {
+    return (
+      <>
+        <h1>Pretty Pictures!</h1>
+        <form onSubmit={this.onFormSubmit}>
+          <input id="query" />
+          <input type="submit" value="search for pretty pictures" />
+          {this.state.pictures ? 
+            this.state.pictures.map(picture => <img key={picture.urls.full} src={picture.urls.thumb} alt={picture.description}/>)
+          : ''}
+        </form>
+      </>
+    )
+  }
 }
 
 export default App;
