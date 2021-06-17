21866e33191b6503c37f8858ee9fb874b9c9336c
mnfmnfm <mnfmnfm@gmail.com>
2021-06-17 13:07:58 -0700

    minor edits

--- src/App.js
+++ src/App.js
@@ -9,15 +9,26 @@ class App extends React.Component {
 
     this.state = {};
   }
-  onFormSubmit = async (e) => {
+  onFormSubmit = (e) => {
     e.preventDefault();
-    let pictureData = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/pictures?searchQuery=${e.target.query.value}`);
-
-    console.log(pictureData);
-
-    this.setState({
-      pictures: pictureData.data
-    });
+    console.log('before the axios.get');
+    axios.get(`${process.env.REACT_APP_BACKEND_URL}/pictures?searchQuery=${e.target.query.value}`)
+      .then(pictureData => {
+        console.log('inside of the .then');
+        console.log(pictureData);
+    
+        this.setState({
+          pictures: pictureData.data
+        });
+      })
+      .catch(err => {
+        console.log('there was an error', err);
+      })
+    console.log('after the axios.get');
+    // you cannot use pictureData here
+    // the data is not back yet
+    // the request is still happening
+    // you will have a bad time
   }
   render() {
     return (
@@ -27,7 +38,7 @@ class App extends React.Component {
           <input id="query" />
           <input type="submit" value="search for pretty pictures" />
           {this.state.pictures ? 
-            this.state.pictures.map(picture => <img key={picture.urls.full} src={picture.urls.thumb} alt={picture.description}/>)
+            this.state.pictures.map(picture => <img key={picture.url} src={picture.url} alt={picture.description}/>)
           : ''}
         </form>
       </>
